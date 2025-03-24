4bbec8a2c855810021f0588af8b6b01ccb57c2f3
Bruno Prado <bruno.BJ684@gmail.com>
2025-03-24 10:42:08 +0000

    editando a classe mentoria

--- src/br/com/bootcamp/Mentoria.java
+++ src/br/com/bootcamp/Mentoria.java
@@ -2,13 +2,24 @@ package br.com.bootcamp;
 
 public class Mentoria extends Conteudo {
 
+    private String dataMentoria;
+
     @Override
     public double calcularXp() {
-        return XP_PADRAO + 20d;
+        return XP_PADRAO + 20;  // A XP da mentoria é sempre XP_PADRAO + 20
+    }
+
+    // Getters e Setters
+    public String getDataMentoria() {
+        return dataMentoria;
+    }
+
+    public void setDataMentoria(String dataMentoria) {
+        this.dataMentoria = dataMentoria;
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        return super.toString() + " | Data da Mentoria: " + dataMentoria;
     }
 }
