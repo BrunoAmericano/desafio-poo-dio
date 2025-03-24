700f815ad96c8ea3e8e4a7f2f0164f76d0777635
Bruno Prado <bruno.BJ684@gmail.com>
2025-03-24 10:42:50 +0000

    editando a classe dev

--- src/br/com/bootcamp/Dev.java
+++ src/br/com/bootcamp/Dev.java
@@ -1,35 +1,50 @@
 package br.com.bootcamp;
 
-import java.util.LinkedHashSet;
-import java.util.Optional;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Dev {
+
     private String nome;
-    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
-    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosInscritos = new HashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new HashSet<>();
+    private double xp;
+
+    public Dev(String nome) {
+        this.nome = nome;
+        this.xp = 0.0;
+    }
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
         this.conteudosInscritos.addAll(bootcamp.getConteudos());
     }
 
     public void progredir() {
-        Optional<Conteudo> conteudo = conteudosInscritos.stream().findFirst();
-        if (conteudo.isPresent()) {
-            conteudosConcluidos.add(conteudo.get());
-            conteudosInscritos.remove(conteudo.get());
+        if (!conteudosInscritos.isEmpty()) {
+            Conteudo conteudo = conteudosInscritos.iterator().next(); // Pega o primeiro conteúdo inscrito
+            conteudosConcluidos.add(conteudo); // Adiciona ao conjunto de concluídos
+            xp += conteudo.calcularXp(); // Acumula XP
+            conteudosInscritos.remove(conteudo); // Remove o conteúdo da lista de inscritos
+        } else {
+            System.out.println("Você não está inscrito em nenhum conteúdo.");
         }
-    }
-
-    public double calcularTotalXp() {
-        return conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public double getXp() {
+        return xp;
+    }
+
+    @Override
+    public String toString() {
+        return "Dev{" +
+                "nome='" + nome + '\'' +
+                ", xp=" + xp +
+                ", conteudosInscritos=" + conteudosInscritos +
+                ", conteudosConcluidos=" + conteudosConcluidos +
+                '}';
     }
 }
